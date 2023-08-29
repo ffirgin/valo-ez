046859760aa030f01d7fa9bca8fecc58f24a31f3
ffirgin <ffirgn@gmail.com>
2023-08-29 14:25:01 -0700

    updated guide page, focusing on other pages before guide page

--- src/app/guides/page.tsx
+++ src/app/guides/page.tsx
@@ -1,8 +1,8 @@
 export default function Guides() {
   return (
-    <div>
-      <h1>Guides</h1>
-      <a href="/guides/lineups">Lineups</a>
+    <div className="flex flex-col items-center justify-between p-24">
+      <h1 className='text-3xl font-extrabold uppercase'>This section is a work in progress</h1>
+      {/* <a href="/guides/lineups">Lineups</a> */}
     </div>
   )
 }
