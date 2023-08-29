f886e546ee43cf150a33eec3df81a4bcd674e89c
ffirgin <ffirgn@gmail.com>
2023-08-29 16:51:49 -0700

    initial setup for agent page with API usage and mapping over data

--- src/app/agents/page.tsx
+++ src/app/agents/page.tsx
@@ -1,7 +1,29 @@
-export default function Agents() {
-  return(
+import Image from "next/image";
+
+const getAgentData = async () => {
+  const res = await fetch("https://valorant-api.com/v1/agents");
+  return res.json();
+};
+
+export default async function Agents() {
+  const agents = await getAgentData();
+  return (
     <div>
       <h1>Agents</h1>
+      <ul>
+        {agents.data.map((agent) => (
+          <>
+            <li>{agent.displayName}</li>
+            <li>{agent.description}</li>
+            <Image
+              src={agent.displayIconSmall}
+              width={50}
+              height={50}
+              alt="Agent icon"
+            />
+          </>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
