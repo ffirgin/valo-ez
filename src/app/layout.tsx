f7f6072542982e3ec7a023bbdb035c32e1cd36be
ffirgin <ffirgn@gmail.com>
2023-08-29 13:25:59 -0700

    changed default font to poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import Navbar from './components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: "600" })
 
 export const metadata: Metadata = {
   title: 'Valo Made EZ',
