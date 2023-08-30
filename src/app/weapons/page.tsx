dcfb3c7bef86121de64928d17dc38ef22eac0d7a
ffirgin <ffirgn@gmail.com>
2023-08-30 14:19:41 -0700

    added api call and basic showing of information from api, will add complexity later

--- src/app/weapons/page.tsx
+++ src/app/weapons/page.tsx
@@ -1,7 +1,32 @@
-export default function Weapons() {
+import Image from "next/image"
+
+type Weapons = {
+
+};
+
+const getWeaponData = async () => {
+  const res = await fetch("https://valorant-api.com/v1/weapons")
+  return res.json();
+}
+
+export default async function Weapons() {
+  const weapons = await getWeaponData();
   return (
     <div>
       <h1>Weapons</h1>
+      <ul>
+        {weapons.data.map((weapon) => (
+          <>
+            <li>{weapon.displayName}</li>
+            <Image 
+              src={weapon.displayIcon}
+              width={200}
+              height={200}
+              alt={weapon.displayName}
+            />
+          </>
+        ))}
+      </ul>
     </div>
   )
 }
