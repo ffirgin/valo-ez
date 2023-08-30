30019fa737492cc1dc78611a21ec54d148d27309
ffirgin <ffirgn@gmail.com>
2023-08-30 13:55:18 -0700

    Added type Agent for strict type relating to API data, also added placeholder portrait for later styling ideas

--- src/app/agents/page.tsx
+++ src/app/agents/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
 
+type Agent = {
+  uuid: string;
+  displayName: string;
+  description: string;
+  isPlayableCharacter: boolean;
+  displayIconSmall: string;
+  displayIcon: string;
+  fullPortrait: string;
+}
+
 const getAgentData = async () => {
   const res = await fetch("https://valorant-api.com/v1/agents");
   return res.json();
 };
+
 
 export default async function Agents() {
   const agents = await getAgentData();
@@ -13,17 +24,23 @@ export default async function Agents() {
       <h1>Agents</h1>
       <ul>
         {agents.data
-          .filter((playable) => playable.isPlayableCharacter)
-          .map((agent) => (
+          .filter((playable: Agent) => playable.isPlayableCharacter)
+          .map((agent: Agent) => (
             <>
               <li>{agent.displayName}</li>
               <Image
-                src={agent.displayIconSmall}
+                src={agent.displayIcon}
                 width={50}
                 height={50}
                 alt="Agent icon"
               />
-              <li>{agent.description}</li>
+              <Image
+                src={agent.fullPortrait}
+                width={400}
+                height={400}
+                alt="Agent full portrait"
+              />
+              <p>{agent.description}</p>
             </>
           ))}
       </ul>
