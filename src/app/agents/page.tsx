9db0bd44d7d7a83f2a64df9f7c1a040160e29683
ffirgin <ffirgn@gmail.com>
2023-08-31 14:02:22 -0700

    styling added to page, sort added to make agents show in alphabetical order, name/role move to under image

--- src/app/agents/page.tsx
+++ src/app/agents/page.tsx
@@ -8,6 +8,7 @@ type Agent = {
   displayIconSmall: string;
   displayIcon: string;
   fullPortrait: string;
+  role: any;
 };
 
 const getAgentData = async () => {
@@ -15,35 +16,47 @@ const getAgentData = async () => {
   return res.json();
 };
 
-
 export default async function Agents() {
   const agents = await getAgentData();
 
   return (
     <div>
-      <h1>Agents</h1>
-      <ul>
+      <h1 className="text-center uppercase text-2xl font-bold pb-5">
+        Agents
+      </h1>
+      <div className="flex flex-wrap gap-4 justify-center items-center pb-5 ">
         {agents.data
           .filter((playable: Agent) => playable.isPlayableCharacter)
+          .sort((a: Agent,b: Agent) => a.displayName.localeCompare(b.displayName))
           .map((agent: Agent) => (
             <>
-              <li>{agent.displayName}</li>
-              <Image
-                src={agent.displayIcon}
-                width={50}
-                height={50}
-                alt="Agent icon"
-              />
-              <Image
-                src={agent.fullPortrait}
-                width={400}
-                height={400}
-                alt="Agent full portrait"
-              />
-              <p>{agent.description}</p>
+              <a
+                href={`/agent/${agent.displayName.toLowerCase()}`}
+                className="flex flex-col max-w-[400px] items-center border-4 hover:border-indigo-500 transition hover:duration-300"
+              >
+                <Image
+                  src={agent.fullPortrait}
+                  width={400}
+                  height={400}
+                  alt="Agent full portrait"
+                  style={{}}
+                />
+                <div className="flex gap-5">
+                  <p>{agent.displayName}</p>
+                  <div className="flex gap-1 items-center">
+                    <Image
+                      src={agent.role.displayIcon}
+                      width={20}
+                      height={20}
+                      alt={`${agent.role.displayName} icon`}
+                    />
+                    <p>{agent.role.displayName}</p>
+                  </div>
+                </div>
+              </a>
             </>
           ))}
-      </ul>
+      </div>
     </div>
   );
 }
