63e372da81bf1c163af88c131087388b838b08f3
ffirgin <ffirgn@gmail.com>
2023-09-01 13:18:17 -0700

    colors on homepage updated to match rest of project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col h-screen items-center justify-center p-24 pt-1">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center cursor-default">
         <h1 className="text-5xl font-extrabold text-center">
           {" "}
           Valorant information, made EZ! 🤓{" "}
@@ -13,11 +13,11 @@ export default function Home() {
           about the agents, weapons and maps of the game! Guides coming soon...
         </p>
         <div className="flex pt-3 items-center">
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-3 rounded mx-4">
+          <button className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-3 rounded mx-4">
             Get Started
           </button>
-          <a className="">
-            Learn more <span>→</span>
+          <a href="/about">
+            Learn more <span className="hover:translate-x-1 ease-in-out duration-200">→</span>
           </a>
         </div>
       </div>
