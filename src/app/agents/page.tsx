5849f356dbbd07ea14be2d591a549e995e936395
ffirgin <ffirgn@gmail.com>
2023-09-01 13:19:01 -0700

    some styling changes added to make agent boxes stand out - will decide what colors to use later

--- src/app/agents/page.tsx
+++ src/app/agents/page.tsx
@@ -34,16 +34,16 @@ export default async function Agents() {
                 href={`/agent/${agent.displayName
                   .replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, "")
                   .toLowerCase()}`}
-                className="flex flex-col max-w-[400px] items-center border-4 hover:border-indigo-500 transition hover:ease-in-out hover:duration-200"
+                className="flex flex-col max-w-[400px] items-center border-2 hover:border-indigo-500 rounded-3xl bg-indigo-100 hover:bg-indigo-400 transition hover:ease-in-out hover:duration-200 z-20 overflow-hidden"
               >
                 <Image
                   src={agent.fullPortrait}
                   width={400}
                   height={400}
                   alt="Agent full portrait"
-                  className="hover:transition hover:duration-200 hover:scale-105 hover:ease-in-out hover:translate-y-1"
+                  className="hover:transition hover:duration-300 hover:scale-105 hover:ease-in-out hover:translate-y-1 z-0"
                 />
-                <div className="flex gap-5 py-2 font-extrabold text-xl">
+                <div className="flex gap-5 py-2 font-extrabold text-xl text-white bg-indigo-500 rounded-t px-3 h-10 items-center z-10">
                   <p>{agent.displayName}</p>
                   <div className="flex gap-1 items-center">
                     <Image
