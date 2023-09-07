7426499d3e396a98bb0954e941f36a4c9df2f334
Griffin <ffirgn@gmail.com>
2023-09-06 17:54:11 -0700

    moving individual agent box to it's own component

---
+++ src/app/components/AgentBox.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export const AgentBox = ({ agent }) => {
+  return (
+    <a
+      href={`/agentData/agents/${agent.displayName
+        .replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, "")
+        .toLowerCase()}`}
+      className="flex flex-col max-w-[400px] items-center border-2 hover:border-indigo-500 rounded-3xl bg-indigo-100 hover:bg-indigo-400 transition hover:ease-in-out hover:duration-200 z-20 overflow-hidden"
+    >
+      <Image
+        src={agent.fullPortrait}
+        width={400}
+        height={400}
+        alt="Agent full portrait"
+        className="hover:transition hover:duration-300 hover:scale-105 hover:ease-in-out hover:translate-y-1 z-0"
+      />
+      <div className="flex gap-5 py-2 font-extrabold text-xl text-white bg-indigo-500 rounded-t px-3 h-10 items-center z-10">
+        <p>{agent.displayName}</p>
+        <div className="flex gap-1 items-center">
+          <Image
+            src={agent.role.displayIcon}
+            width={20}
+            height={20}
+            alt={`${agent.role.displayName} icon`}
+          />
+          <p>{agent.role.displayName}</p>
+        </div>
+      </div>
+    </a>
+  );
+};
