48203f7b078f76e4639fd5bf966f612b5e7c85ec
Griffin <ffirgn@gmail.com>
2023-09-13 12:42:28 -0700

    added stopPropagation to make the icons not clickable to avoid filter being empty. also added styling to make the icons fall in line with the text

--- src/app/components/AgentsFilter.tsx
+++ src/app/components/AgentsFilter.tsx
@@ -6,27 +6,65 @@ type Props = {
 
 const AgentsFilter: React.FC<Props> = ({ setFilteredAgents }) => {
   return (
-    <div className="flex justify-center items-center gap-4">
-      <button onClick={setFilteredAgents} value="All">
+    <div className="flex justify-center items-center gap-4 mb-5">
+      <button onClick={setFilteredAgents} value="All" className="hover:bg-indigo-300 rounded px-1">
         All
       </button>
-      <button onClick={setFilteredAgents} value="Controller">
+      <button
+        onClick={setFilteredAgents}
+        value="Controller"
+        className="flex flex-row-reverse justify-center items-center gap-1 hover:bg-indigo-300 rounded px-1"
+      >
         Controller
         <Image
           src="/controller-icon.png"
           alt="Controller Icon"
-          width={30}
-          height={30}
+          width={20}
+          height={20}
+          onClick={(e) => e.stopPropagation()}
         />
       </button>
-      <button onClick={setFilteredAgents} value="Duelist">
+      <button
+        onClick={setFilteredAgents}
+        value="Duelist"
+        className="flex flex-row-reverse justify-center items-center gap-1 hover:bg-indigo-300 rounded px-1"
+      >
         Duelist
+        <Image
+          src="/duelist-icon.png"
+          alt=""
+          width={20}
+          height={20}
+          onClick={(e) => e.stopPropagation()}
+        />
       </button>
-      <button onClick={setFilteredAgents} value="Initiator">
+      <button
+        onClick={setFilteredAgents}
+        value="Initiator"
+        className="flex flex-row-reverse justify-center items-center gap-1 hover:bg-indigo-300 rounded px-1"
+      >
         Initiator
+        <Image
+          src="/initiator-icon.png"
+          alt=""
+          width={20}
+          height={20}
+          onClick={(e) => e.stopPropagation()}
+        />
       </button>
-      <button onClick={setFilteredAgents} value="Sentinel">
+      <button
+        onClick={setFilteredAgents}
+        value="Sentinel"
+        className="flex flex-row-reverse justify-center items-center gap-1 hover:bg-indigo-300 rounded px-1"
+      >
         Sentinel
+        <Image
+          src="/sentinel-icon.png"
+          alt=""
+          width={20}
+          height={20}
+          onClick={(e) => e.stopPropagation()}
+        />
       </button>
     </div>
   );
