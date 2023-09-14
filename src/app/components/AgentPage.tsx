719dc84c83cd84bb59de8f2754e018b11e956fc6
ffirgin <ffirgn@gmail.com>
2023-09-14 11:59:41 -0700

    added agent image and started styling of abilities

--- src/app/components/AgentPage.tsx
+++ src/app/components/AgentPage.tsx
@@ -25,28 +25,35 @@ const AgentPage: React.FC<Props> = ({ agentId }) => {
   return (
     <>
       {/* <div>Hello World {JSON.stringify(agentData)}</div> */}
-      <div>
+      <div className="min-h-screen">
         <div>
           {agentData.map((agent: Agent) => (
-            <div key={agent.uuid} className="">
-              <div>{agent.displayName}</div>
+            <div key={agent.uuid} className="flex flex-col items-center">
+              <h1 className="font-bold text-2xl pb-5">{agent.displayName}</h1>
+              <Image
+                src={agent.displayIcon}
+                width={100}
+                height={100}
+                alt={`${agent.displayName}'s Icon`}
+                className="mb-5"
+              />
               <div>
-                Abilities
-                <div>
+                <h2 className="text-center text-xl">Abilities</h2>
+                <div className="flex flex-col items-center text-center max-w-2xl">
                   {agent.abilities.map((abilities: Agent) => (
-                    <>
-                      <div>{abilities.displayName}</div>{" "}
+                    <div className="flex flex-col items-center mb-4" key={agent.uuid}>
+                      <h3 className="underline mt-3">{abilities.displayName}</h3>{" "}
                       {abilities.displayIcon !== null && (
                         <Image
                           alt={`${agent.displayName}'s Ability Icon`}
                           src={abilities.displayIcon}
                           width={50}
                           height={50}
-                          className="bg-gray-400 w-[75px] object-scale-down"
+                          className="bg-gray-400 w-[75px] object-scale-down mt-1"
                         />
                       )}
-                      <div>{abilities.description}</div>
-                    </>
+                      <p className="mt-1">{abilities.description}</p>
+                    </div>
                   ))}
                 </div>
               </div>
