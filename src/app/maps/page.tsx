a717f58d6472daa237ffae2fd102df8f284fd93b
Griffin <ffirgn@gmail.com>
2023-09-22 15:30:31 -0700

    added full map list with filter and styling

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,35 +1,43 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
 type Maps = {
   uuid: string;
   displayName: string;
   listViewIcon: string;
+  splash: string;
+  map: string;
+  xMultiplier: number;
 };
 
 const getMapData = async () => {
-  const res = await fetch("https://valorant-api.com/v1/maps")
+  const res = await fetch("https://valorant-api.com/v1/maps");
   return res.json();
-}
+};
 
 export default async function Maps() {
   const maps = await getMapData();
 
-  return(
-    <div>
-      <h1>Maps</h1>
-      <ul>
-        {maps.data.map((map: Maps) => (
-          <>
-            <li>{map.displayName}</li>
-            <Image 
-              src={map.listViewIcon}
-              width={400}
-              height={400}
-              alt="Map"
-            />
-          </>
-        ))}
-      </ul>
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-center uppercase text-2xl font-bold pb-5">Maps</h1>
+      {/* MapsView */}
+      <div className="flex flex-wrap justify-center gap-4 mb-5">
+        {maps.data
+          .filter((map: Maps) => map.xMultiplier !== 0)
+          .map((map: Maps) => (
+            <div key={map.uuid}>
+              <h2 className="text-center">{map.displayName}</h2>
+              <Image
+                src={map.splash}
+                width={500}
+                height={300}
+                alt={`${map.displayName}'s smaller icon`}
+                className="hover:drop-shadow-md"
+              />
+            </div>
+          ))}
+      </div>
     </div>
-  )
+  );
 }
