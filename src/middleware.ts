ec9d7bb160e3fa54826793fd33df11a014af5087
Griffin Hobbs <ffirgn@gmail.com>
2024-02-16 11:16:48 -0800

    added public routes to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,16 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/login"],
+  publicRoutes: [
+    "/",
+    "/login",
+    "/signup",
+    "/agents",
+    "/maps",
+    "/(maps|agents)/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/",
+    "/agents/:param+",
+    "/maps/:param+", //Clerk uses :param+ to match one or more characters after a specified route
+  ],
 });
 
 export const config = {
