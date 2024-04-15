47ced80ec1da43a2979bdee869ce6821674812c1
Griffin Hobbs <ffirgn@gmail.com>
2024-04-15 11:53:26 +0800

    removed animation for agent title at top of page

--- src/app/agents/page.tsx
+++ src/app/agents/page.tsx
@@ -1,6 +1,4 @@
-"use client"
 import AgentsView from "../components/AgentsView";
-import { motion } from "framer-motion";
 
 const getAgentsData = async () => {
   const res = await fetch("https://valorant-api.com/v1/agents", {
@@ -15,27 +13,7 @@ export default async function Agents() {
 
   return (
     <div className="">
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.01,
-            },
-          },
-        }}
-      >
-        <h1 className="text-center uppercase text-2xl font-bold pb-5">
-          Agents
-        </h1>
-      </motion.div>
+      <h1 className="text-center uppercase text-2xl font-bold pb-5">Agents</h1>
       <AgentsView data={data.data} />
     </div>
   );
